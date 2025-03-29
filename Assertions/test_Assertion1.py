38cfdc2a4e91bd10045b0b965bcfbd33056f9972
Ajjayya G V <ajjayya2002@gmail.com>
2025-03-29 23:32:05 +0530

    initial commit

---
+++ Assertions/test_Assertion1.py
@@ -0,0 +1,36 @@
+'''
+
+IN Pytest Assertions are the  primary way to test  conditions, Python's built in assert statement 
+is used to compare expected and actual outcomes , if assertion fails ,pytest provides a detailed failure report
+
+'''
+import pytest
+
+#Basic Assertions in pytest
+
+def test_add():
+    assert 2 + 3 == 5
+    assert 5 + 2 == 7
+
+
+def test_example():
+    a = 5
+    b = 10
+    # assert a > b
+    # assert b > a
+
+
+def  test_subtract():
+    result =  10 - 5
+    assert result == 5 , "Expected  result is 5 , but  got 4 "
+
+
+#pytest.raises()
+
+def divide(a,b):
+    a / b
+
+def  test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(10,0)    
+
